8424e30d10e6e1b0453b9a5989fde85dd32ef976
Santi Vidal <m0n5xhx@gmail.com>
2023-08-28 20:27:13 -0300

    starting-fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const INITIAL_LIMIT = 8;
 export const SHIPPING_COST = 18;
-export const BASE_URL = 'https://integrador-react-zeta.vercel.app/';
+export const BASE_URL = 'https://cors.iamnd.eu.org/?url=https://integrador-react-zeta.vercel.app/';
